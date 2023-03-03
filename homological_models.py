da89d895cf06673922515c8c166ae957ad35f851
Antonio Briola <Antonio>
2023-03-03 15:38:22 +0000

    State-of-the-art tied.

--- homological_models.py
+++ homological_models.py
@@ -729,7 +729,8 @@ class HCNN_model1D(LightningModule):
 
     def configure_optimizers(self):
         optimizer = torch.optim.AdamW(self.parameters(), self.lr)
-        return optimizer
+        scheduler = torch.optim.lr_scheduler.CosineAnnealingLR(optimizer, T_max=32)
+        return [optimizer], [scheduler]
 
     def set_lr(self, lr):
         self.lr = lr
